71a9f1b9af2f8c2daf76109109684470fba80656
Valeco96 <correalev2@gmail.com>
2025-10-02 21:30:28 +0200

    Created routes

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,16 @@
 import express from "express";
-import "dotenv/config";
+import cors from "cors"; // permette di gestire il CORS (chiamate da FE su indirizzi diversi da quello del BE)
+import "dotenv/config"; // importa il contenuto del file env
 
 const port = process.env.PORT;
-const server = express();
+const server = express(); // creo il server base
+server.use(cors()); // accetta richieste da qualsiasi indirizzo - non specificato 
 
-server.listen(port, () => console.log(`Server avviato sulla porta ${port}`));
+//indirizzo di esempio per provare che funzioni
+server.get("/", (request, response) => response.send("heyyy"));
+server.get("/bo", (request, response) => response.send("theereee"));
+server.get("/username", (request, response) =>
+  response.send({ username: "suzu" })
+);
+
+server.listen(port, () => console.log(`Server avviato sulla porta ${port}`)); // il server é in ascolto di richieste alla porta indicata
