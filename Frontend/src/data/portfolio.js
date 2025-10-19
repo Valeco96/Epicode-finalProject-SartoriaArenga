d273e32cf3ccf4af84d697dad869594d6e1c7666
Valeco96 <correalev2@gmail.com>
2025-10-19 06:39:25 +0200

    Connected FE and BE for portfolio through axios and data folder

---
+++ Frontend/src/data/portfolio.js
@@ -0,0 +1,79 @@
+import axios from "./axios.js";
+
+export async function getAllPieces() {
+  try {
+    const response = await axios.get("/api/portfolio");
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function createPiece(newPiece, token) {
+  try {
+    const response = await axios.post(`/api/portfolio`, newPiece, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getSinglePiece(id, token) {
+  try {
+    const response = await axios.get(`/api/portfolio/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+    console.log("Response completa:", response.data);
+  } catch (error) {
+    console.log("Errore getSinglePiece:", error);
+  }
+}
+
+export async function editPiece(id, pieceData, token) {
+  try {
+    const response = await axios.put(`/api/portfolio/${id}`, pieceData, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  } catch (error) {
+    console.error("Errore editPiece:", error);
+    throw error;
+  }
+}
+
+export async function deletePiece(pieceId, token) {
+  try {
+    const response = await axios.delete(`/api/portfolio/${pieceId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+export const updateImage = async (id, imageFile, token) => {
+  try {
+    const formData = new FormData();
+    formData.append("image", imageFile);
+    const response = await axios.patch(`/api/portfolio/${id}/image`, formData, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
